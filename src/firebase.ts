24929d45d82c2476603e718e6e2e1bcf4693838d
ArzyVX <arrrazy@gmail.com>
2026-05-16 18:01:06 +0800

    arrazy back end databse for program cards ez

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAIlWM-SYPkW-ZvORYAx3Ey3IStyXafDFw",
@@ -14,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
